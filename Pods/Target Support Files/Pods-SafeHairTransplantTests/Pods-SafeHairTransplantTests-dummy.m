5413ed7839c9c24171065cd316a9a2d5ffa583d0
Niharika Sharma <niharikagarg40@gmail.com>
2017-08-05 21:26:32 -0400

    installing pods

---
+++ Pods/Target Support Files/Pods-SafeHairTransplantTests/Pods-SafeHairTransplantTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SafeHairTransplantTests : NSObject
+@end
+@implementation PodsDummy_Pods_SafeHairTransplantTests
+@end
---
+++ Pods/Target Support Files/Pods-SafeHairTransplantUITests/Pods-SafeHairTransplantUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SafeHairTransplantUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SafeHairTransplantUITests
+@end
